95ada4c75fd1529893f2b2b35dfc87263cd1937f
Fredrick Anigbo <anigbofredrick@gmail.com>
2022-07-08 12:54:44 +0100

    Create main.js
    i added a Javascript file to it

---
+++ main.js
@@ -0,0 +1,83 @@
+function validate() {
+    let fname = document.getElementById("fname").value
+    let error1 = document.getElementById("error1")
+     let lname = document.getElementById("lname").value
+    let error2 = document.getElementById("error2")
+    let password = document.getElementById("password").value
+    let error3 = document.getElementById("error3")
+    let email = document.getElementById("email").value
+    let error4 = document.getElementById("error4")
+    
+    document.getElementById("fname").style.backgroundImage = "url(https://res.cloudinary.com/dwomgx0nz/image/upload/v1655225239/icon-error_r0k1t8.svg)"
+    document.getElementById("fname").style.backgroundRepeat = "no-repeat"
+    document.getElementById("fname").style.backgroundPosition = "right center"
+    document.getElementById("fname").style.backgroundOrigin = "content-box"
+    
+
+    document.getElementById("lname").style.backgroundImage = "url(https://res.cloudinary.com/dwomgx0nz/image/upload/v1655225239/icon-error_r0k1t8.svg)"
+    document.getElementById("lname").style.backgroundRepeat = "no-repeat"
+    document.getElementById("lname").style.backgroundPosition = "right center"
+    document.getElementById("lname").style.backgroundOrigin = "content-box"
+
+    document.getElementById("email").style.backgroundImage = "url(https://res.cloudinary.com/dwomgx0nz/image/upload/v1655225239/icon-error_r0k1t8.svg)"
+    document.getElementById("email").style.backgroundRepeat = "no-repeat"
+    document.getElementById("email").style.backgroundPosition = "right center"
+    document.getElementById("email").style.backgroundOrigin = "content-box"
+    
+
+    document.getElementById("password").style.backgroundImage = "url(https://res.cloudinary.com/dwomgx0nz/image/upload/v1655225239/icon-error_r0k1t8.svg)"
+    document.getElementById("password").style.backgroundRepeat = "no-repeat"
+    document.getElementById("password").style.backgroundPosition = "right center"
+    document.getElementById("password").style.backgroundOrigin = "content-box"
+    
+
+  
+    
+    if(fname.length == "") {
+    error1.textContent = "first name cannot be empty"
+    //return false
+    
+    
+    }
+     
+    else {
+        error1.textContent = ""
+        
+    }
+    
+    if(lname.length == "") {
+    error2.textContent = "last name cannot be empty"
+    //return false
+  }
+  
+  else {
+    error2.textContent = ""
+    //return false
+  }
+ 
+  if(email.length == "") {
+    error3.textContent = "looks like this is not an email"
+    //return false
+  }
+  
+  else {
+    error3.textContent = ""
+    //return false
+  }
+  
+  if(password.length == "") {
+    error4.textContent = "password cannot be empty"
+    return false
+  }
+  
+  else {
+    error4.textContent = ""
+   return false
+  }
+ 
+
+  
+    
+  
+}
+
